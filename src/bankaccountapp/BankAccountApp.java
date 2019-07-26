f7eb1fc1b46a2bc27fafb0d86aad23fe576e72bc
asalia-1 <asalia@wesleyan.edu>
2019-07-26 00:38:04 -0400

    Updating files

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -6,7 +6,15 @@ public class BankAccountApp {
     
     public static void main(String[] args) {
         
-        Checking chk1 = new Checking("Tom Wilson");
+        Checking chk1 = new Checking("Tom Wilson", "234867498", 1789);
+        
+        Savings sav1 = new Savings("Abdallah Salia", "576830456", 6758);
+        
+        sav1.showInfo();
+        System.out.println("**************************");
+        chk1.showInfo();
+        
+        sav1.compound();
         
         // Read a csv file and generate new accounts based on that data
         
